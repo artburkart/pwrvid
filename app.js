8966f6865539f310c88d3f91c8beda101d224932
Arthur Burkart <artburkart@gmail.com>
2014-07-11 23:59:25 -0500

    socket io

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*jslint browser: true, indent: 4 */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -9,10 +11,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 4000);
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -25,7 +30,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -36,7 +41,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -47,7 +52,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -55,4 +60,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-module.exports = app;
+module.exports = {app: app, server: server};
