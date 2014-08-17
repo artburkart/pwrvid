ba6af884e8c69f6645311fc05cc7eba5802660df
Arthur Burkart <artburkart@gmail.com>
2014-08-16 22:13:52 -0500

    switched to Packt book by Sergiienko

--- routes/pwrvid.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET pwrvid page. */
-router.get('/', function (req, res) {
-  res.render('pwrvid', { title: 'PWRVID' });
-});
-
-module.exports = router;
